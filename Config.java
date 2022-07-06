992f05c1777542c84ba5f3d017bb55933b44aef7
ljl <lijiulin@bupt.edu.cn>
2022-07-06 16:11:06 +0800

    7.6 final version

--- Config.java
+++ Config.java
@@ -4,6 +4,6 @@ public class Config {
     public static int port=53;
     public static String dataBasePath="dnsrelay.txt";
     public static int DebuggerMode=3; //默认正常模式，不进行打印
-
     public static int debuggerCounter=0;
 }
+
